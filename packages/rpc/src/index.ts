4c109f309d04e8bc5015c76344df26b84c04675a
Brian Faust <hello@basecode.sh>
2019-05-31 16:38:29 +0300

    feat: allow setting a custom logger

--- packages/rpc/src/index.ts
+++ packages/rpc/src/index.ts
@@ -1,17 +1,20 @@
 import { Server } from "@hapi/hapi";
 import { startServer } from "./server";
 import { database as db } from "./services/database";
+import { logger } from "./services/logger";
 
 export * from "./interfaces";
 
-export const start = async ({
-    database,
-    server,
-}: {
+export const start = async (options: {
     database: string;
     server: Record<string, any>;
+    logger?: any;
 }): Promise<Server> => {
-    db.connect(database);
+    db.connect(options.database);
 
-    return startServer(server);
+    if (options.logger) {
+        logger.setLogger(options.logger);
+    }
+
+    return startServer(options.server);
 };
--- packages/rpc/src/services/logger.ts
+++ packages/rpc/src/services/logger.ts
@@ -1,7 +1,39 @@
 import pino from "pino";
 
-export const logger = pino({
-    name: "exchange-json-rpc",
-    safe: true,
-    prettyPrint: true,
-});
+class Logger {
+    private logger: any;
+
+    public constructor() {
+        this.logger = pino({
+            name: "exchange-json-rpc",
+            safe: true,
+            prettyPrint: true,
+        });
+    }
+
+    public setLogger(logger: any): void {
+        this.logger = logger;
+    }
+
+    public error(message: any): void {
+        this.logger.error(message);
+    }
+
+    public warn(message: any): void {
+        this.logger.warn(message);
+    }
+
+    public info(message: any): void {
+        this.logger.info(message);
+    }
+
+    public debug(message: any): void {
+        this.logger.debug(message);
+    }
+
+    public verbose(message: any): void {
+        this.logger.verbose(message);
+    }
+}
+
+export const logger = new Logger();
